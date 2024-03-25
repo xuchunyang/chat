a47515fb9d9eb75e56b80d12c89f8612abc20b5a
Xu Chunyang <xuchunyang56@gmail.com>
2024-03-25 10:59:12 +0800

    Disable model event when seeding

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -17,13 +17,15 @@ class RoomSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create 5 rooms with a user
-        Room::factory()
-            ->count(5)
-            ->has(Message::factory()->count(10)->for(User::factory()))
-            ->for(User::factory()->state([
-                'password' => 'secret_pass_123',
-            ]))
-            ->create();
+        $this->withoutModelEvents(function () {
+            // Create 5 rooms with a user
+            Room::factory()
+                ->count(5)
+                ->has(Message::factory()->count(10)->for(User::factory()))
+                ->for(User::factory()->state([
+                    'password' => 'secret_pass_123',
+                ]))
+                ->create();
+        });
     }
 }
